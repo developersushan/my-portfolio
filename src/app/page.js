532c279b810fafbf0969002bdcb6173da2ed9716
developersushan <sushansing709sushansing@gmail.com>
2024-07-11 23:47:24 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <h5 className="text-3xl font-bold ">welcome portfolio website</h5>
     </main>
   );
